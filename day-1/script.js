ee45f7914bec391960fd6b07d74ccb5de9072f07
Felipe Oliveira <115155241+Code-Lipe@users.noreply.github.com>
2023-03-22 14:28:49 -0300

    Upgrate java.script

--- day-1/script.js
+++ day-1/script.js
@@ -7,31 +7,31 @@ let stringTen = '10';
 
 
 if(numberOne === stringOne){
-    console.log('The variables numberOne and stringOne have the same value and the same type.');
+    console.log("The variables numberOne and stringOne have the same value and the same type.");
 }
 else if (numberOne == stringOne) {
-    console.log('The numberOne and stringOne variables have the same value but different types.');
+    console.log("The numberOne and stringOne variables have the same value but different types.");
 } 
 else {
-    console.log('The numberOne and stringOne variables do not have the same value.');
+    console.log("The numberOne and stringOne variables do not have the same value.");
 };
 
 if (numberThirty === stringThirty) {
-    console.log('The variables numberThirty and stringThirty have the same value and the same type.');;
+    console.log("The variables numberThirty and stringThirty have the same value and the same type.");
 }
 else if(numberThirty == stringThirty){
-    console.log('The numberThirty and stringThirty variables have the same value but different types.');
+    console.log("The numberThirty and stringThirty variables have the same value but different types.");
 }
 else {
-    console.log('The numberThirty and stringThirty variables do not have the same value.');
+    console.log("The numberThirty and stringThirty variables do not have the same value.");
 };
 
 if (numberTen === stringTen) {
-    console.log('The variables numberTen and strinrTen have the same value and the same type.');;
+    console.log("The variables numberTen and strinrTen have the same value and the same type.");
 }
 else if(numberTen == stringTen){
-    console.log('The numberTen and strinrTen variables have the same value but different types.');
+    console.log("The numberTen and strinrTen variables have the same value but different types.");
 }
 else {
-    console.log('The numberTen and strinrTen variables do not have the same value.');
+    console.log("The numberTen and strinrTen variables do not have the same value.");
 };
