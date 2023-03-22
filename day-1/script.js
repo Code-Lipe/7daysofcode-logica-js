d9fb3db4e16e1d709b8888a8ea4abecbeda318a7
Felipe Oliveira <Felipe.OSantos@hotmail.com>
2023-03-22 14:24:25 -0300

    day-1 complete

---
+++ day-1/script.js
@@ -0,0 +1,37 @@
+let numberOne = 1;
+let stringOne = '1';
+let numberThirty = 30;
+let stringThirty = '30';
+let numberTen = 10;
+let stringTen = '10';
+
+
+if(numberOne === stringOne){
+    console.log('The variables numberOne and stringOne have the same value and the same type.');
+}
+else if (numberOne == stringOne) {
+    console.log('The numberOne and stringOne variables have the same value but different types.');
+} 
+else {
+    console.log('The numberOne and stringOne variables do not have the same value.');
+};
+
+if (numberThirty === stringThirty) {
+    console.log('The variables numberThirty and stringThirty have the same value and the same type.');;
+}
+else if(numberThirty == stringThirty){
+    console.log('The numberThirty and stringThirty variables have the same value but different types.');
+}
+else {
+    console.log('The numberThirty and stringThirty variables do not have the same value.');
+};
+
+if (numberTen === stringTen) {
+    console.log('The variables numberTen and strinrTen have the same value and the same type.');;
+}
+else if(numberTen == stringTen){
+    console.log('The numberTen and strinrTen variables have the same value but different types.');
+}
+else {
+    console.log('The numberTen and strinrTen variables do not have the same value.');
+};
