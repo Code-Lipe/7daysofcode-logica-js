303cab6a8e8fcbc7a840525d3b3189511974d4f5
Felipe Oliveira <115155241+Code-Lipe@users.noreply.github.com>
2023-03-23 23:34:41 -0300

    Update script.js
    Refactoring, decreasing lines and repetitions

--- day-1/script.js
+++ day-1/script.js
@@ -5,33 +5,23 @@ let stringThirty = '30';
 let numberTen = 10;
 let stringTen = '10';
 
-
-if(numberOne === stringOne){
-    console.log("The variables numberOne and stringOne have the same value and the same type.");
-}
-else if (numberOne == stringOne) {
-    console.log("The numberOne and stringOne variables have the same value but different types.");
+if (numberOne == stringOne) {
+  console.log('Variables numberOne and stringOne have the same value but different types');
 } 
 else {
-    console.log("The numberOne and stringOne variables do not have the same value.");
+  console.log('Variables numberOne and stringOne do not have the same value');
 };
 
 if (numberThirty === stringThirty) {
-    console.log("The variables numberThirty and stringThirty have the same value and the same type.");
-}
-else if(numberThirty == stringThirty){
-    console.log("The numberThirty and stringThirty variables have the same value but different types.");
-}
+  console.log('The variables numberThirty and stringThirty have the same value and same type');
+} 
 else {
-    console.log("The numberThirty and stringThirty variables do not have the same value.");
+  console.log('The variables numberThirty and stringThirty do not have the same type');
 };
 
-if (numberTen === stringTen) {
-    console.log("The variables numberTen and strinrTen have the same value and the same type.");
-}
-else if(numberTen == stringTen){
-    console.log("The numberTen and strinrTen variables have the same value but different types.");
-}
+if (numberTen == stringTen) {
+  console.log('Variables numberTen and stringTen have the same value, but different types');
+} 
 else {
-    console.log("The numberTen and strinrTen variables do not have the same value.");
+  console.log('The variables numTen and stringTen do not have the same value');
 };
